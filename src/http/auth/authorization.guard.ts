474b2703a3290d2b1939249b9f1866ea33891e79
Felipe Pinheiro <felipe.pinheiro@cotefacil.com>
2025-04-03 18:57:01 -0300

    chore: add a authorization guard

--- src/http/auth/authorization.guard.ts
+++ src/http/auth/authorization.guard.ts
@@ -5,9 +5,39 @@ import {
   UnauthorizedException,
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { NextFunction, Response, Request } from 'express'
-import { auth } from 'express-oauth2-jwt-bearer'
+import { Request } from 'express'
+import {
+  auth,
+  type AuthOptions,
+  type AuthResult,
+} from 'express-oauth2-jwt-bearer'
 
+export const jwtVerifier = (config?: Omit<AuthOptions, 'authRequired'>) => {
+  const middleware = auth(config)
+
+  return (accessToken?: string): Promise<AuthResult> => {
+    return new Promise((resolve, reject) => {
+      const request = {
+        query: { access_token: accessToken },
+        headers: {},
+        is: () => false,
+        auth: undefined as unknown as AuthResult,
+      }
+
+      void middleware(request as any, {} as any, (error: unknown) => {
+        if (error) {
+          reject(
+            error instanceof Error
+              ? error
+              : new Error(JSON.stringify(error, null, 2)),
+          )
+        } else {
+          resolve(request.auth)
+        }
+      })
+    })
+  }
+}
 @Injectable()
 export class AuthorizationGuard implements CanActivate {
   private AUTH0_AUDIENCE: string
@@ -21,17 +51,17 @@ export class AuthorizationGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const httpContext = context.switchToHttp()
     const req = httpContext.getRequest<Request>()
-    const res = httpContext.getResponse<Response>()
-    const next = httpContext.getNext<NextFunction>()
 
-    const jwtCheck = auth({
+    const jwtCheck = jwtVerifier({
       audience: this.AUTH0_AUDIENCE,
       issuerBaseURL: this.AUTH0_DOMAIN,
       tokenSigningAlg: 'RS256',
     })
 
+    const token = req.headers.authorization?.split(' ')[1]
+
     try {
-      await jwtCheck(req, res, next)
+      await jwtCheck(token)
       return true
     } catch (err) {
       throw new UnauthorizedException(err)
